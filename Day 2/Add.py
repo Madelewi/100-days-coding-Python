cc6d20381581b3dcaf0d547cbab2618d08922f0f
Madelewi <149729465+Madelewi@users.noreply.github.com>
2023-11-22 06:50:11 +0100

    A program that adds the digits in a 2 digit number

---
+++ Day 2/Add.py
@@ -0,0 +1,6 @@
+two_digit_number = input('input a two digit number: ')
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+# two_digit_number1 = int(two_digit_number)
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
