722e3723224ecfa81568c47886688de5708e54d5
Madelewi <149729465+Madelewi@users.noreply.github.com>
2023-11-27 17:43:40 +0100

    Your Life in Weeks calculator

---
+++ Day 2/Life in weeks.py
@@ -0,0 +1,7 @@
+age = input("input your age ")
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+age_int = int(age)
+years = 90 - age_int
+weeks = years * 52
+print(f"You have {weeks} weeks left.")
